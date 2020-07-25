2f32e5b882bd1b8887a69737b3e376d1e6dca2ec
Mohammad Reza AliAbadi <mohammadrezaaliabadi9@gmail.com>
2020-07-25 23:01:12 +0430

    add merge sort algorithm

---
+++ db/src/main/java/is/db/datastructure/MergeSort.java
@@ -0,0 +1,97 @@
+package is.db.datastructure;
+
+
+public class MergeSort<T extends Comparable>
+{
+
+    private final T[] items, aux;
+
+    public MergeSort(T[] items)
+    {
+        this.items = items;
+
+        /* We have to make aux a distinct array from items */
+        this.aux = (T[]) new Comparable[items.length];
+    }
+
+    /**
+     * Method that initiates the sort the given items array
+     */
+    public void sort()
+    {
+        this.sort(0, this.items.length - 1);
+    }
+
+    private void sort(int low, int high)
+    {
+        if (low >= high)
+        {
+            /* Were done sorting this section, do nothing */
+            return;
+        }
+
+        /* Compute the midpoint */
+        int mid = low + ((high - low) / 2);
+
+        /* Sort the first half */
+        this.sort(low, mid);
+
+        /* Sort the second half */
+        this.sort(mid + 1, high);
+
+        /* Merge the 2 halves */
+        this.merge(low, mid, high);
+    }
+
+    /**
+     * Merge the values from low - high into the main array
+     *
+     * @param low  The starting point of the merge
+     * @param mid  The dividing index to divide the arrays by
+     * @param high The ending point of the merge
+     */
+    private void merge(int low, int mid, int high)
+    {
+        int i = low;        // Index value in main array
+
+        int j = low;        // Index to the first half of the aux array
+        int k = mid + 1;    // Index to the second half of the aux array
+
+        /* Update the aux Array */
+        for (int x = low; x <= high; x++)
+        {
+            this.aux[x] = this.items[x];
+        }
+
+        while (j <= mid || k <= high)
+        {
+            if (j > mid)
+            {
+                /* The first half is already finished, lets just go through the second half */
+                this.items[i] = this.aux[k++];
+            }
+            else if (k > high)
+            {
+                /* The second half is already finished, lets just go through the first half */
+                this.items[i] = this.aux[j++];
+            }
+            else if (this.aux[j].compareTo(this.aux[k]) > 0)
+            {
+                /* If aux[j] is > aux[k] */
+                this.items[i] = this.aux[k++];
+            }
+            else
+            {
+                /* If aux[j] is <= aux[k] */
+                this.items[i] = this.aux[j++];
+            }
+
+            i++;    // Increment the position we're at in the main array
+        }
+    }
+
+    public T[] getSortedItems()
+    {
+        return this.items;
+    }
+}
---
+++ db/src/test/java/is/db/datastructure/MergeSortTest.java
@@ -0,0 +1,27 @@
+package is.db.datastructure;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MergeSortTest {
+
+    @Test
+    void sort() {
+        Integer[] items =
+                {
+                        8, 10, 13, 5, 14, 11, 7, 12, 1, 4, 17, 20, 18, 6, 9, 15, 19, 2, 16
+                };
+
+        /* Create a new instance of the mergesort Algorithm and sort the array of integers */
+        MergeSort<Integer> mergeSort = new MergeSort<>(items);
+        mergeSort.sort();
+
+        /* Printing the array to check whether they are in sorted order */
+        System.out.println("Printing Sorted Items: ");
+        for (Integer x : mergeSort.getSortedItems()) {
+            System.out.print(x + " ");
+        }
+    }
+
+}
